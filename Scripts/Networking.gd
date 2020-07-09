0df5d301988d51f2659f27c2001aaa016f83f416
Cheuk Yin Ng <chucksys88@gmail.com>
2020-07-09 13:44:33 -0700

    feat: add networking and transforms WIP

--- Scripts/Networking.gd
+++ Scripts/Networking.gd
@@ -6,9 +6,11 @@ signal paddle_info
 
 onready var Utils = load("res://Scripts/Utils.gd").new()
 
-const SERVER_PORT = 15267
+const SERVER_PORT = 12345
 var CLIENT_IP = null
 var CLIENT_PORT = 0
+var acc = Vector3(0, 0, 0)
+var rot = [1, 1, 1, 0, 0]
 
 var socketUDP = PacketPeerUDP.new()
 
@@ -16,11 +18,23 @@ func _ready():
 	start_server()
 
 func _process(delta):
-	if socketUDP.get_available_packet_count() > 0:
-		var bytes = socketUDP.get_packet()
+	while socketUDP.get_available_packet_count() > 0:
+		var buffer = StreamPeerBuffer.new()
+		buffer.big_endian = true
+		buffer.set_data_array(socketUDP.get_packet())
+		var numElements = buffer.get_u32()
+		acc.x = buffer.get_float()
+		acc.y = buffer.get_float()
+		acc.z = buffer.get_float()
+		rot[0] = buffer.get_float()
+		rot[1] = buffer.get_float()
+		rot[2] = buffer.get_float()
+		rot[3] = buffer.get_float()
+		rot[4] = buffer.get_float()
+
 		CLIENT_IP = socketUDP.get_packet_ip()
 		CLIENT_PORT = socketUDP.get_packet_port()
-		print(CLIENT_IP, "-", CLIENT_PORT, ":", bytes.get_string_from_ascii())
+#		print(CLIENT_IP, "-", CLIENT_PORT, ":", numElements)
 
 func _exit_tree():
 	print('Shutting down the server...')
--- Scripts/Play Area.gd
+++ Scripts/Play Area.gd
@@ -1,9 +1,15 @@
 extends Spatial
 
 onready var Networking = load("res://Scripts/Networking.gd").new()
+onready var Paddle1 = get_node("Paddle 1")
+onready var Paddle2 = get_node("Paddle 2")
 
 func _ready():
 	Networking._ready()
 	
 func _process(delta):
 	Networking._process(delta)
+	
+#	var v = Vector3(Networking.rot[1], Networking.rot[2], Networking.rot[0])
+#	Paddle1.set_rotation(v)
+	Paddle1.move_and_slide(Networking.acc * delta * 100)
